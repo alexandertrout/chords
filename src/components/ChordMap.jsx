33c5e10651fd1c5ac9e4a9bffb33f5ec2ed33556
Alexander Trout <alexandertrout@A1.local>
2020-03-24 13:06:22 +0000

    fixed keys

--- src/components/ChordMap.jsx
+++ src/components/ChordMap.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MapImage from "../MapImage.png"
+import MapImage from "../MapImage2.png"
 
 class ChordMap extends Component {
   state = {
@@ -12,12 +12,12 @@ class ChordMap extends Component {
         'G',
         'G',
         'A min'],
-      D: ['D', 'E min', 'E min', 'F min', 'G', 'A', 'B', 'B min'],
-      E: ['E', 'F min', 'F min', 'G min', 'A', 'B', 'C', 'C min'],
-      F: ['F', 'G min', 'G min', 'A min', 'B', 'C', 'D', 'D min'],
-      G: ['G', 'A min', 'A min', 'B min', 'C', 'D', 'E', 'E min'],
-      A: ['A', 'B min', 'B min', 'C min', 'D', 'E', 'F', 'F min'],
-      B: ['B', 'C min', 'C min', 'D min', 'E', 'F', 'G', 'G min']
+      D: ['D', 'E min', 'E min', 'F# min', 'G', 'A', 'A', 'B min'],
+      E: ['E', 'F# min', 'F# min', 'G# min', 'A', 'B', 'B', 'C# min'],
+      F: ['F', 'G min', 'G min', 'A min', 'Bb', 'C', 'C', 'D min'],
+      G: ['G', 'A min', 'A min', 'B min', 'C', 'D', 'D', 'E min'],
+      A: ['A', 'B min', 'B min', 'C# min', 'D', 'E', 'E', 'F# min'],
+      B: ['B', 'C# min', 'C# min', 'D# min', 'E', 'F#', 'F#', 'G# min']
     },
     key: 'c',
     chords: []
